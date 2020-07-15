3723b4ce7d7e6ccbd62b1f752cc446b3cdf63064
Derrick Nguyen <derrickhnguyen@outlook.com>
2020-07-15 12:06:15 -0700

    Implemented profile image url upload

--- src/pages/me.tsx
+++ src/pages/me.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Router from 'next/router'
+import Link from 'next/link'
 import { format } from 'date-fns'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
@@ -25,17 +26,73 @@ const AboutSection = ({ currentUser }: AboutSectionProps) => {
   const { firstName, lastName, profile } = currentUser || {}
   const [isEditBioInputOpen, setIsEditBioInputOpen] = React.useState(false)
   const [bio, setBio] = React.useState(profile?.bio)
+  const [profileImageUrl, setProfileImageUrl] = React.useState(
+    profile?.profileImageUrl
+  )
+
+  const fileUploaderRef = React.useRef<HTMLInputElement>()
+
   const [
     updateCurrentUserInformation
   ] = useUpdateCurrentUserInformationMutation()
 
+  const openProfilePictureUploader = () => fileUploaderRef.current.click()
+  const uploadProfilePicture = async (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const [profilePicture] = Array.from(e.target.files)
+    const data = new FormData()
+    data.append('file', profilePicture)
+    data.append('upload_preset', 'blogr_app')
+
+    const res = await fetch(
+      'https://api.cloudinary.com/v1_1/derrickhnguyen/image/upload',
+      {
+        method: 'POST',
+        body: data
+      }
+    )
+    const file = await res.json()
+
+    await updateCurrentUserInformation({
+      variables: {
+        input: {
+          updatableCurrentUserInformation: { profileImageUrl: file.secure_url }
+        }
+      }
+    })
+
+    setProfileImageUrl(file.secure_url)
+  }
+
   return (
     <section className="bg-white flex flex-col p-6 lg:w-2/6 shadow-2xl lg:mr-3 lg:mb-0 mb-3">
-      <img
-        alt="sample"
-        src="https://source.unsplash.com/random/300x300"
-        className="mb-5 m-auto"
+      {profileImageUrl && (
+        <button
+          className="mb-5 flex mx-auto focus:outline-none focus:shadow-outline hover:opacity-75"
+          onClick={openProfilePictureUploader}
+        >
+          <img alt={`${firstName} ${lastName}`} src={profileImageUrl} />
+        </button>
+      )}
+      {!profileImageUrl && (
+        <div>
+          <button
+            className="mb-5 hover:bg-gray-100 hover:text-gray-600 text-gray-700 flex h-56 w-auto p-3 mx-auto justify-center text-center items-center content-center border border-gray-700 border-solid focus:outline-none focus:shadow-outline"
+            onClick={openProfilePictureUploader}
+          >
+            Click here to upload a profile picture
+          </button>
+        </div>
+      )}
+      <input
+        ref={fileUploaderRef}
+        id="profile-picture-uploader"
+        className="hidden"
+        type="file"
+        onChange={uploadProfilePicture}
       />
+
       <h2 className="text-xl mb-2 underline text-center lg:text-left">
         {firstName} {lastName}
       </h2>
@@ -117,18 +174,17 @@ const PostSection = () => {
       </div>
       <div className="flex flex-wrap">
         {results.map(({ createdAt, title, id }) => (
-          <button
-            key={id}
-            className="flex flex-col bg-white border focus:outline-none focus:shadow-outline shadow-lg p-4 w-full lg:w-posts m-2 hover:bg-gray-100"
-          >
-            {title && <h3 className="text-xl">{title}</h3>}
-            {!title && (
-              <h3 className="text-xl italic text-gray-700">No Title</h3>
-            )}
-            <div className="text-sm italic text-gray-700">
-              Created on: {format(new Date(createdAt), 'yyyy-MM-dd')}
-            </div>
-          </button>
+          <Link key={id} href="/post">
+            <a className="flex flex-col bg-white border focus:outline-none focus:shadow-outline shadow-lg p-4 w-full lg:w-posts m-2 hover:bg-gray-100">
+              {title && <h3 className="text-xl">{title}</h3>}
+              {!title && (
+                <h3 className="text-xl italic text-gray-700">No Title</h3>
+              )}
+              <div className="text-sm italic text-gray-700">
+                Created on: {format(new Date(createdAt), 'yyyy-MM-dd')}
+              </div>
+            </a>
+          </Link>
         ))}
       </div>
     </section>
